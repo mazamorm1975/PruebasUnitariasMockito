3ea72b2cbd1d750514df2969184260ebd0c94ddd
mazamora1975 <mzamora114@hotmail.com>
2024-01-04 14:28:47 -0600

    Se agrego dependencia para plugin de maven surfire 2.22.2

--- src/test/java/org/mockito/test_doubles/fake/FakeTest.java
+++ src/test/java/org/mockito/test_doubles/fake/FakeTest.java
@@ -1,8 +1,6 @@
 package org.mockito.test_doubles.fake;
 
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
-
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Collection;
